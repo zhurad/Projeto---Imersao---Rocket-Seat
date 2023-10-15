255bdedb040572a3d9c8f2a977a5eb42dd9af17f
Zhurad <fernando.ft.thompson@gmail.com>
2023-10-15 17:46:07 -0300

    Dockerfile deploy

---
+++ src/main/java/org/zhurad/todolist/users/IUserRepository.java
@@ -0,0 +1,11 @@
+package org.zhurad.todolist.users;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    UserModel findByUsername(String username);
+}
+
